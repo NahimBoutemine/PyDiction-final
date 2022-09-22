7e8d1a47cd8f670a253f0f5fb23ff598a085b989
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-09-22 20:33:18 +0200

    Update streamcode_PyDiction2.py

--- streamcode_PyDiction2.py
+++ streamcode_PyDiction2.py
@@ -374,6 +374,7 @@ if rad == "Evaluation de la performance des modèles pré-sélectionnés":
   #rfc = RandomForestClassifier(max_depth = 8, n_estimators = 200, criterion = 'gini', max_features = 'sqrt')
   
   model = model1#initialisation du modèle (il faut un premier choix initial, changeable ensuite)
+  #chargement des joblib : fonctionne en local, concernant l'application en ligne il semble que les joblib font "planter", en effet en local le code fonctionne et la mémoire de streamlit en ligne semble saturer en version gratuite.
   filename1 = "KNNbest_pipeline_opti.joblib"
   model1 = joblib.load(filename1)
 
